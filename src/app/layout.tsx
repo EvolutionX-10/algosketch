43bedb20b7c5f6ac47bdebb3ff7f180a67d3a663
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2025-04-30 00:11:39 +0530

    chore: add favicon (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,20 @@ const fancy = Gloria_Hallelujah({
 export const metadata: Metadata = {
 	title: "AlgoSketch",
 	description: "A sketchbook for algorithms",
+	icons: {
+		icon: [
+			{
+				media: "(prefers-color-scheme: light)",
+				url: "/images/icon-light.png",
+				href: "/images/icon-light.png",
+			},
+			{
+				media: "(prefers-color-scheme: dark)",
+				url: "/images/icon.png",
+				href: "/images/icon-dark.png",
+			},
+		],
+	},
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
