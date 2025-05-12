89596cd2400c3f7d398e790242e92d717ab575ad
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2025-05-12 21:50:31 +0530

    feat: add bubble sort algorithm text

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,40 @@
 import type { MDXComponents } from "mdx/types";
+import { Prism } from "react-syntax-highlighter";
+import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { GeistMono } from "geist/font/mono";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
+		code: ({ children, className, ...props }) => {
+			const language = className?.replace(/language-/, "") || "text";
+			if (language === "text") {
+				return (
+					<span
+						className={"rounded-md bg-gray-900 p-1 text-sm text-gray-500 dark:text-gray-300 " + GeistMono.className}
+						{...props}
+					>
+						{children}
+					</span>
+				);
+			}
+			return (
+				<Prism
+					language={language}
+					style={coldarkDark}
+					codeTagProps={{
+						style: {
+							fontFamily: GeistMono.style.fontFamily,
+						},
+					}}
+					{...props}
+				>
+					{children}
+				</Prism>
+			);
+		},
+		pre: ({ children }) => {
+			return children;
+		},
 		...components,
 	};
 }
