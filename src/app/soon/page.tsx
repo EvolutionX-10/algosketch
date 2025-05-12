9ff4e31b615509c8e0c7e0d018a525fbe742043f
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2025-05-12 16:32:01 +0530

    chore: move grid lines to soon page

--- src/app/soon/page.tsx
+++ src/app/soon/page.tsx
@@ -1,3 +1,7 @@
 export default function Soon() {
-	return <h1 className="font-heading z-10 text-8xl font-bold">Coming Soon</h1>;
+	return (
+		<main className="grid-lines flex h-screen w-screen flex-1 items-center justify-center">
+			<h1 className="font-heading z-10 text-8xl font-bold">Coming Soon</h1>
+		</main>
+	);
 }
