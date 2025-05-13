6a70b7b6c0469317d999ec813e5a937a977fb421
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2025-05-13 11:10:05 +0530

    feat: allocate space for visualizer and banner

---
+++ src/components/bubble/visualizer.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRef } from "react";
+import Banner from "../banner";
+
+export default function Visualizer() {
+	const ref = useRef<HTMLDivElement | null>(null);
+	const handleClick = () => {
+		if (ref && ref.current) {
+			ref.current.scrollIntoView({ behavior: "smooth" });
+		}
+	};
+
+	return (
+		<>
+			<Banner onClickAction={handleClick} />
+			<div ref={ref}>hello world incoming</div>
+		</>
+	);
+}
