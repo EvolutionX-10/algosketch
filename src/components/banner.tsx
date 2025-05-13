ff2354080e1f1ee1e44773d8f3c08f87899a3fe8
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2025-05-13 11:11:18 +0530

    chore: unh
    permalink: http://whatthecommit.com/fe87f02a27818e6c8614eb0dba00be00

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -4,7 +4,7 @@ export default function Banner({ onClickAction }: BannerProps) {
 	return (
 		<div className="absolute top-4 left-0 flex w-full items-center justify-center">
 			<button
-				className="font-fancy mx-2 w-full max-w-[75ch] cursor-pointer rounded-md border-2 bg-blue-50/50 px-4 py-1 text-lg font-bold uppercase shadow-md transition duration-200 ease-in-out hover:shadow-lg focus:outline-none active:scale-95"
+				className="font-fancy mx-2 w-full max-w-[75ch] cursor-pointer rounded-md border-2 px-4 py-1 text-lg font-bold uppercase shadow-md transition duration-200 ease-in-out hover:shadow-lg focus:outline-none active:scale-95"
 				onClick={onClickAction}
 			>
 				Go to Visualizer!
