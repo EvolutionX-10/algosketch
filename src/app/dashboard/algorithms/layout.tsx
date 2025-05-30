450a5958549950dc99531baa8ff5add6d1b6c59b
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2025-05-30 17:19:23 +0530

    fix: remove console log for pathname in MdxLayout component

--- src/app/dashboard/algorithms/layout.tsx
+++ src/app/dashboard/algorithms/layout.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
-	console.log("Current pathname:", pathname);
+
 	if (pathname === "/dashboard/algorithms/sorting") return children;
 	return (
 		<div className="prose prose-headings:mt-8 prose-headings:font-semibold prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-invert">
