4229090e966bfce40b6ae0f321afdbc7a234609b
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2025-06-13 13:03:35 +0530

    fix: allow local development requests to bypass middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+	if (request.nextUrl.origin === "http://localhost:3000") return NextResponse.next();
+
 	const adminPasswordPath = "/password";
 	const clearAdminPath = "/clear";
 
