22de1f45e0427f2a82bd228f6d3a0fb72239c947
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2025-06-28 16:58:18 +0530

    chore: remove unused imports from utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { navData } from "./constants";
-import { type NextRequest, NextResponse } from "next/server";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
