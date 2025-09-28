98e5f5f6096e58a7cf47e82ebcbb7d6950bb2298
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2025-09-28 09:27:46 +0530

    chore: remove Soon component and its associated page

--- src/app/soon/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Soon() {
-	return (
-		<div className="grid-lines flex min-h-screen w-full flex-col">
-			<main className="flex flex-1 items-center justify-center">
-				<h1 className="font-heading z-10 text-8xl font-bold">Coming Soon</h1>
-			</main>
-		</div>
-	);
-}
